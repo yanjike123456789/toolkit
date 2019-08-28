745196b62df7e827970117410a5fbfe54dff29ec
yanjike123456789 <53998029+yanjike123456789@users.noreply.github.com>
2019-08-28 19:39:10 +0800

    2

--- Hold-all.js
+++ Hold-all.js
@@ -1,24 +1,24 @@
 //获取验证码时间
 // 注册事件
-function Verificati(element) {
-  // 定义一个变量
-  let num = 60
-  // 使用禁止按键
-  element.disabled = true;
-  // 为了保证点击瞬间就计时,先在点的瞬间修改文字一次
-  element.value = '获取验证码(' + num + ')';
-  // 每隔一段时间修改文字
-  let intervalId = setInterval(function () {
-    num--;
-    element.value = '获取验证码(' + num + ')';
-    // 如果时间到了0了,需要停止
-    if (num === 0) {
-      clearInterval(intervalId)
-      element.disabled = false;
-      element.value = '获取验证码'
-    }
-  }, 1000)
-}
+// kits.Verificati = function (element, num) {
+//   // 定义一个变量
+//   let num = 60
+//   // 使用禁止按键
+//   element.disabled = true;
+//   // 为了保证点击瞬间就计时,先在点的瞬间修改文字一次
+//   element.value = '获取验证码(' + num + ')';
+//   // 每隔一段时间修改文字
+//   let intervalId = setInterval(function () {
+//     num--;
+//     element.value = '获取验证码(' + num + ')';
+//     // 如果时间到了0了,需要停止
+//     if (num === 0) {
+//       clearInterval(intervalId)
+//       element.disabled = false;
+//       element.value = '获取验证码'
+//     }
+//   }, 1000)
+// }
 
 var kits = {};
 //获取年月日时分秒：
@@ -38,15 +38,14 @@ kits.formatDate = function () {
   return year + '-' + montht + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 
 }
-// kits.randomInt = function (n, m) {
-//   return Math.floor(Math.random() * (m - n + 1) + n);
-// }
-
+kits.randomInt = function (n, m) {
+  return Math.floor(Math.random() * (m - n + 1) + n);
+}
 
 
 
 //调用移动端的点击事件
-function tap(element, fn) {
+kits.tap = function (element, fn) {
   let dat;
   // 定义变量位置
   let startX, startY;
@@ -73,11 +72,38 @@ function tap(element, fn) {
     let endX = e.changedTouches[0].pageX;
     let endY = e.changedTouches[0].pageY;
 
-    if (Math.abs(endX - startX) > 50 || Math.abs(endX - startY) > 50) {
+    if (Math.abs(endX - startX) > 50 || Math.abs(endY - startY) > 50) {
       console.log('偏移过大')
       return;
     }
 
   })
   fn && fn()
+}
+
+
+// 随机颜色
+kits.getRandomColor = function () {
+  return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);
+}
+
+
+
+// 本地存储
+kits.getArray = function (key) {
+  let json = localStorage.getItem(key);
+  let arr = JSON.parse(json);
+  return arr || [];
+}
+
+
+/**
+ * @description 封装好的把复杂数据存储到本地里面的方法，默认是存储json格式字符串
+ * @param {string} key 存储到本地里面的键
+ * @param {object} obj 要存储的复杂数据
+ * @returns undefined
+ */
+kits.setData = function (key, obj) {
+  let json = JSON.stringify(obj)
+  localStorage.setItem(key, json)
 }
